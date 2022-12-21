0f87790f115c06c86b7d0fc5095ee94a1d61334b
minoru-github <kurami0621@yahoo.co.jp>
2022-12-21 23:35:12 +0900

    feat: base logic of client

--- socket-cpp.cpp
+++ socket-cpp.cpp
@@ -1,6 +1,46 @@
 ﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+
+#pragma comment(lib,"Ws2_32.lib")
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    WSADATA data{};
+    const auto err_startup = WSAStartup(MAKEWORD(2, 0), &data);
+    if (err_startup) {
+        std::cerr << "can't startup. error: " + std::to_string(err_startup);
+        return err_startup;
+    }
+
+    const char* hostname = "localhost";
+    const char* service = "80";
+    addrinfo hints{};
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_INET;
+
+    addrinfo* res = nullptr;
+    const auto err_getaddrinfo = getaddrinfo(hostname, service, &hints, &res);
+    if (err_getaddrinfo) {
+        std::cerr << "can't getaddrinfo. error: " + std::to_string(err_getaddrinfo);
+        return err_getaddrinfo;
+    }
+
+    const SOCKET sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "can't create socket.";
+        return 1;
+    }
+
+    const auto err_connect = connect(sock, res->ai_addr, static_cast<int>(res->ai_addrlen));
+    if (err_connect) {
+        std::cerr << "can't connect. error: " + std::to_string(err_connect);
+        return err_connect;
+    }
+
+    freeaddrinfo(res);
+    WSACleanup();
+
+    return 0;
 }

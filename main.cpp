d69c0e1b2d42438eff4532cee248700132d9dec6
minoru-github <kurami0621@yahoo.co.jp>
2022-12-22 23:52:41 +0900

    refactor: impl receive_msg()

--- main.cpp
+++ main.cpp
@@ -6,19 +6,32 @@
 #pragma comment(lib,"Ws2_32.lib")
 
 const int initialize_winsock();
-void finalize_socket_communication(const SOCKET sock);
+void finalize_socket_communication(
+    const SOCKET sock
+);
 const sockaddr_in create_ip_address();
 const SOCKET create_socket();
-const int bind(const SOCKET listen_sock);
-const int listen(const SOCKET listen_sock);
-const SOCKET accept(const SOCKET listen_sock);
-const int connect_to_server(const SOCKET sock);
+const int bind(
+    const SOCKET listen_sock
+);
+const int listen(
+    const SOCKET listen_sock
+);
+const SOCKET accept(
+    const SOCKET listen_sock
+);
+const int connect_to_server(
+    const SOCKET sock
+);
 void server();
 void client();
 
 const int send_msg(
     const SOCKET sock,
     const std::string msg
+);
+const std::string receive_msg(
+    const SOCKET sock
 );
 
 int main()
@@ -37,7 +50,7 @@ const int initialize_winsock() {
     // https://learn.microsoft.com/ja-jp/windows/win32/winsock/initializing-winsock
     const int err_startup = WSAStartup(version_requested, &data);
     if (err_startup) {
-        std::cerr << "can't startup. error: " + std::to_string(err_startup);
+        std::cerr << "can't startup. error: " + std::to_string(err_startup) << std::endl;
     }
     return err_startup;
 }
@@ -62,12 +75,14 @@ const sockaddr_in create_ip_address()
     return addr;
 }
 
-const int bind(const SOCKET listen_sock) {
+const int bind(
+    const SOCKET listen_sock
+) {
     const auto addr = create_ip_address();
 
     const int error = bind(listen_sock, (sockaddr*)&addr, static_cast<int>(sizeof(addr)));
     if (error == SOCKET_ERROR) {
-        std::cerr << "can't bind. error: " + std::to_string(WSAGetLastError());
+        std::cerr << "can't bind. error: " + std::to_string(WSAGetLastError()) << std::endl;
         finalize_socket_communication(listen_sock);
     }
     return error;
@@ -80,28 +95,32 @@ const SOCKET create_socket() {
 
     const SOCKET sock = socket(address_family, type, protocol);
     if (sock == INVALID_SOCKET) {
-        std::cerr << "can't create socket. error: " + std::to_string(WSAGetLastError());
+        std::cerr << "can't create socket. error: " + std::to_string(WSAGetLastError()) << std::endl;
         WSACleanup();
     }
     return sock;
 }
 
-const int listen(const SOCKET listen_sock) {
+const int listen(
+    const SOCKET listen_sock
+) {
     // TODO 第二引数のbacklogの意味調べる
     const int err = listen(listen_sock, 1);
     if (err == SOCKET_ERROR) {
-        std::cerr << "can't listen. error: " + std::to_string(WSAGetLastError());
+        std::cerr << "can't listen. error: " + std::to_string(WSAGetLastError()) << std::endl;
         finalize_socket_communication(listen_sock);
     }
 
     return err;
 }
 
-const SOCKET accept(const SOCKET listen_sock) {
+const SOCKET accept(
+    const SOCKET listen_sock
+) {
     std::cerr << "Waiting for client to connect..." << std::endl;
     const SOCKET accpet_socket = accept(listen_sock, NULL, NULL);
     if (accpet_socket == INVALID_SOCKET) {
-        std::cerr << "can't accpet. error: " + std::to_string(WSAGetLastError());
+        std::cerr << "can't accpet. error: " + std::to_string(WSAGetLastError()) << std::endl;
         finalize_socket_communication(listen_sock);
     }
     else {
@@ -111,12 +130,14 @@ const SOCKET accept(const SOCKET listen_sock) {
     return accpet_socket;
 }
 
-const int connect_to_server(const SOCKET sock) {
+const int connect_to_server(
+    const SOCKET sock
+) {
     const auto addr = create_ip_address();
 
     const int error = connect(sock, (sockaddr*)&addr, static_cast<int>(sizeof(addr)));
     if (error) {
-        std::cerr << "can't connect. error: " + std::to_string(WSAGetLastError());
+        std::cerr << "can't connect. error: " + std::to_string(WSAGetLastError()) << std::endl;
         finalize_socket_communication(sock);
     }
     return error;
@@ -128,12 +149,33 @@ const int send_msg(
 ) {
     const int error = send(sock, msg.c_str(), static_cast<int>(msg.length()), 0);
     if (error == SOCKET_ERROR) {
-        std::cerr << "can't send message. error: " + std::to_string(WSAGetLastError());
+        std::cerr << "can't send message. error: " + std::to_string(WSAGetLastError()) << std::endl;
         finalize_socket_communication(sock);
     }
     return error;
 }
 
+const std::string receive_msg(
+    const SOCKET sock
+) {
+    // TODO 汎用性高める
+    constexpr size_t default_buflen = 512;
+    char recvbuf[default_buflen] = { 0 };
+    int recvbuflen = default_buflen;
+
+    const int received_size = recv(sock, recvbuf, recvbuflen, 0);
+
+    if (received_size == 0) {
+        finalize_socket_communication(sock);
+    }
+    else if (received_size < 0) {
+        std::cerr << "can't receive message. error: " + std::to_string(WSAGetLastError()) << std::endl;
+        finalize_socket_communication(sock);
+    }
+
+    const std::string msg = recvbuf;
+    return msg;
+}
 
 void client() {
     const int err_initialize = initialize_winsock();
@@ -189,23 +231,10 @@ void server() {
     char recvbuf[default_buflen] = {0};
     int recvbuflen = default_buflen;
     while (1) {
-        const int received_size = recv(sock, recvbuf, recvbuflen, 0);
-        std::cerr << "received size: " + std::to_string(received_size) << std::endl;
-        if (received_size > 0) {
-            const std::string msg = recvbuf;
+        const std::string msg = receive_msg(sock);
+        if (msg.length() > 0) {
             std::cerr << "received msg: " + msg << std::endl;
-            const int err_send = send_msg(sock, msg + "\n");
-            if (err_send) {
-                return;
-            }
-        }
-        else if (received_size == 0) {
-            finalize_socket_communication(sock);
-            break;
-        }
-        else {
-            std::cerr << "can't receive message. error: " + std::to_string(WSAGetLastError());
-            finalize_socket_communication(sock);
+        } else {
             break;
         }
     }
